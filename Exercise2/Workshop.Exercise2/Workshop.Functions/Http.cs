b8739e33ed75013a568719df126019b556676e0a
Kamil Mrzygłód <kamz@demant-technology.com>
2018-01-20 10:58:10 +0100

    Updated Exercise2

---
+++ Exercise2/Workshop.Exercise2/Workshop.Functions/Http.cs
@@ -0,0 +1,21 @@
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace Workshop.Functions
+{
+    public static class Http
+    {
+        [FunctionName("Http")]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        {
+            log.Info("C# HTTP trigger function processed a request.");
+
+            return req.CreateResponse(HttpStatusCode.OK, "Hello!");
+        }
+    }
+}
